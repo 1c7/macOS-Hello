129d8e17812441945c0b93ade051b2b6bc317356
1c7 <guokrfans@gmail.com>
2016-11-15 19:29:46 +0800

    Initial Commit

---
+++ play/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  play
+//
+//  Created by haha on 2016/11/15.
+//  Copyright © 2016年 haha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ play/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  play
+//
+//  Created by haha on 2016/11/15.
+//  Copyright © 2016年 haha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
